b816821a8ff99201fb322c0e4cf529ea928152ef
dax-side <damolaadegbite0@gmail.com>
2026-01-29 11:18:07 +0100

    chore: add Jest configuration for testing with TypeScript

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  coverageDirectory: 'coverage',
+  verbose: true,
+  bail: false,
+  errorOnDeprecated: true,
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
